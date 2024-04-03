a46234e2a3ef8c12ea9e8ed3da9118a3e1f47401
hesam <hanvarifar@gmail.com>
2024-04-03 18:41:05 +0430

    add channel serializer

--- server/serializers.py
+++ server/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from server.models import Category, Server
+from server.models import Category, Server, Channel
+
+
+class ChannelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Channel
+        fields = ["id", "name", "owner", "topic", "server", "is_active", "created_time", "updated_time"]
 
 
 class ServerSerializer(serializers.ModelSerializer):
+    channel_detail = ChannelSerializer(many=True)
+
     class Meta:
         model = Server
-        fields = ["id", "name", "category", "description", "member", "is_active"]
+        fields = ["id", "name", "category", "description", "member", "is_active", "created_time", "updated_time"]
